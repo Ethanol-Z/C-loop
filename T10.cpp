5382316d69e6ab9b737411845c9b04616142af5b
Ethanol14514 <86411424+Ethanol14514@users.noreply.github.com>
2024-11-20 12:00:39 +0800

    Update T10.cpp

--- T10.cpp
+++ T10.cpp
@@ -3,11 +3,10 @@ using namespace std;
 void T10() {
     for (int i = 0; i <= 33; i++) {
         for (int j = 0; j <= 50; j++) {
-            for (int k = 0; k <= 200; k++) {
-                if (3*i+2*j+k/2 == 100 && i+j+k == 100) {
+            int k = 100 - i - j;
+                if (3*i+2*j+k/2 == 100 && j % 2 == 0) {
                     cout << "Big horses: "<< i << " , medium horses: "<< j << " , small horses: "<< k << endl;
                 }
-            }
         }
     }
 }
